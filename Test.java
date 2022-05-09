fe9378fb6a2bf49588627de4ca1c584cdd28761b
Insung0424 <zhrudrhks@gmail.com>
2022-05-09 18:28:11 +0900

    2022/05/09 d2,beakjoon

--- Test.java
+++ Test.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Test {
 
 	public static void main(String[] args) {
@@ -72,7 +70,194 @@ public class Test {
 //		else if의 조건식 확인 true이면 실행문장2 실행 후 if-else 벗어남
 //		else if의 조건식이 false 이면 
 //		else 를 실행
+//		
+//		Scanner sc = new Scanner(System.in);
+//		char grade;
+//		System.out.println("Enter a number : ");
+//		int score = sc.nextInt();
+//		if(score>=90) {	// 90 <= score
+//			grade = 'A';
+//		}
+//		else if (score>=80) { // 80 <= score < 90
+//			grade = 'B';
+//		}
+//		else if (score>=70) { // 70 <= score < 80
+//			grade = 'C';
+//		}
+//		else {	// score < 70
+//			grade = 'F';
+//		}
+//		
+//		System.out.println("your grade is: "+grade);
+//		
+//		i = a>b ? a-b : b-a; 삼항조건연산자
+//		if(a>b)
+//			i=a-b;
+//		else
+//			i=b-a;
 		
+//		int score = 85;
+//		if(score>=90) 
+//			System.out.println("A");
+//		else
+//			System.out.println("B");
+//		
+//		int score = 85;
+//		char grade = score>90 ? 'A':'B'; //삼항조건연산자
+//		System.out.println(grade);
 		
+//		int total = 300;
+//		double avg = total/3;
+//		
+//		if(avg>=90) {	// 90 <= avg
+//			System.out.println("A");
+//		}
+//		else if (avg>=80 && avg<90) { // 80 <= avg < 90
+//			System.out.println("B");
+//		}
+//		else if (avg>=70) { // 70 <= avg < 80
+//			System.out.println("C");
+//		}
+//		else {	// avg < 70
+//			System.out.println("F");
+//		}
+		
+//		int a=20,b=10;
+//		if(a>10) { //선행조건 바깥쪽 if문 조건이 참이어야 안쪽 if esle확인 가능
+//			if(b>=0) {
+//				b=1;
+//			}
+//			else {
+//				b=-1;
+//			}
+//		}
+//		System.out.println(a+" "+b);
+		
+//		int age=25,kg=60;
+//		char size;
+//		
+//		if(age<20) {
+//			if(kg<50) 
+//				size = 'S';
+//			else if (kg<60)
+//				size = 'M';
+//			else 
+//				size = 'L';
+//		}
+//		else { //age>=20
+//			if(kg<60)
+//				size = 'S';
+//			else if(kg<70)
+//				size = 'M';
+//			else
+//				size = 'L';
+//		}
+//		System.out.println(size);
+		
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("Enter a score: ");
+//		int score = s.nextInt();
+//		
+//		System.out.println("Enter the grade");
+//		int year = s.nextInt();
+//		
+//		// score >= 60 && year != 3
+//		if(score>=60) {
+//			if(year!=3) {
+//				System.out.println("Pass");
+//			}
+//			else if(score>=70) { // score >= 60 && year == 3 && score >= 70
+// 				System.out.println("Pass");
+//			}
+//			else { // score >= 60 && year == 3 && score < 70
+//				System.out.println("Failed");
+//			}
+//		}
+//		else { // score < 60
+//			System.out.println("Failed");
+//		}
+		
+//		// switch ~ case 중 case 에는 상수값만 올 수 있다(문자,문자열,정수)
+		// switch(조건식)  (조건식)에는 다른값가능
+		// switch문은 식의 결과값을 case문과 비교해줘야한다
+//		char grade = 'C';
+//		switch (grade) {
+//			case 'A':
+//			case 'B':
+//				System.out.println("Good");
+//				break; //escape
+//			case 'C':
+//				System.out.println("Not bad");
+//				break;
+//			case 'F':
+//				System.out.println("Bad");
+//				break;
+//		} 
+		
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("What day is it today?");
+//		String day = s.next();
+//		switch(day) {
+//		case "Monday":
+//			System.out.println("Monday");
+//			break;
+//		case "Tuesday":
+//			System.out.println("Tuesday");
+//			break;
+//		default :
+//			System.out.println("Not Monday or Tuesday");
+//			break;
+//		}
+		
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("Enter the grade");
+//		String grade = s.next();
+//		switch(grade) {
+//		case "A":
+//		case "B":
+//			System.out.println("Excellent");
+//			break;
+//		case "C":
+//			System.out.println("Good");
+//			break;
+//		case "D":
+//			System.out.println("Good");
+//			break;
+//		case "F":
+//			System.out.println("Bad");
+//			break;
+//		}
+		
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("What do you do on weekends?");
+//		System.out.println("You can choose one of these: movie, sing, book or other");
+//		String hobby = s.next();
+//		switch(hobby) {
+//		case "movie":
+//			System.out.println("Watch a movie");
+//			break;
+//		case "sing":
+//			System.out.println("Sing a song");
+//			break;
+//		case "book":
+//			System.out.println("Read a book");
+//			break;
+//		default:
+//			System.out.println("etc.");
+//			break;
+//		}
+		
+		//Math.random();
+		// 0.0 <= Math.random() < 1
+
+		//		Math.random()*100
+		// 0.0 <= Math.random() < 100
+
+		//		Math.random() + 1
+		// 1.0 <= Math.random() < 2
+
+		//		(int)(Math.random()*100)+1
+		//		1 <= Math.random() < 100  1부터 100까지 랜덤한범위를 지정할 때 
+
 	}
 }

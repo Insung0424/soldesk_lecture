b0dc61bf72480e15a96bc2d83082a43f60f97068
Insung0424 <zhrudrhks@gmail.com>
2022-05-09 13:16:38 +0900

    first commit

---
+++ Test.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+//		if(조건식){
+//		실행문장
+//		}
+		
+//		Scanner sc= new Scanner(System.in);
+//		System.out.println("정수를 입력해주세요");
+//		int score = sc.nextInt();
+//		if(score>=80) 
+//			System.out.println("Pass");
+//		
+//		else
+//			System.out.println("Failed");
+		
+//		int a=10,b=4;
+//		if(a==10) {
+//			System.out.println("A");
+//		}
+//		if(a>b) {
+//			System.out.println("C");
+//		}
+		// 각각의 if문이 독립적이므로 둘 모두 실행되며
+		// 조건이 전부 '참(true)'으로 두 조건문 다 동작한다
+		
+//		if(조건식){
+//		실행문장1
+//		}
+//		else {
+//		실행문장2	
+//		}
+//		조건식이 true이면 실행문장1 실행 후 if-else 벗어남
+//		false 이면 실행문장2 실행 후 if-else 벗어남
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter a number");
+//		int n = sc.nextInt();
+//		//입력한 수가 9의 배수인지 알고싶다.
+//		if(n%9==0) {
+//			System.out.println("It's a multiple of 9");
+//		}
+//		else {
+//			System.out.println("It's not a multiple of 9");
+//		}
+		
+//		int a=10,b=4;
+//		
+//		if(a==10) {
+//			System.out.println("A");
+//		}
+//		else if(a>b) {
+//			System.out.println("B");
+//		}
+//		else {
+//			System.out.println("C");
+//		}
+//		if(조건식1){
+//		실행문장1
+//		}
+//		else if(조건식2){
+//		실행문장2	
+//		}
+//		else){
+//		실행문장3	
+//		}
+//		if 조건식이 true이면 실행문장1 실행 후 if-else 벗어남
+//		if 조건식이 false 이면 
+//		else if의 조건식 확인 true이면 실행문장2 실행 후 if-else 벗어남
+//		else if의 조건식이 false 이면 
+//		else 를 실행
+		
+		
+	}
+}

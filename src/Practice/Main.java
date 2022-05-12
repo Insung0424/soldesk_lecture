6eb12ed207b94b8db24a3787e787c6fe99f747ca
Insung0424 <zhrudrhks@gmail.com>
2022-05-12 14:23:43 +0900

    10871

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -1,9 +1,41 @@
 package Practice;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		//입력을 위한 버퍼리더
+		StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+		//버퍼리더에서 읽어온 값에서 공백을 기준으로 나누기위한 StringTokenizer
+		int N = Integer.parseInt(st.nextToken());
+		//첫번째 기준 n을 만들기 위해 StringTokenizer.nextToken
+		int X = Integer.parseInt(st.nextToken());
+		//공백기준으로 2번째 , st의 첫번째 입력값 사용 완료
+		StringBuilder sb = new StringBuilder();
+		//문자열을 조합하기 위한 StringBuilder
+		st = new StringTokenizer(bf.readLine());
+		// 두번째 입력을 위한 StringTokenizer
+		for(int i=0;i<N;i++) {
+		// 입력받은 값을 쪼개기 위한 for
+			int v = Integer.parseInt(st.nextToken()); //
+			// 값을 저장하는게 아니라 비교한후 조건에 맞는 것만 출력을 위해 임시적 저장변수
+			if(v<X) {
+			// 문제의 조건
+				sb.append(v+" ");
+				// StringBuilder에서 문자열을 연결시키는 함수
+			}
+		}
+		
+		System.out.println(sb);
+		//결과물
 	}
 
 }
--- src/SolDesk/ClassFive.java
+++ src/SolDesk/ClassFive.java
@@ -83,18 +83,121 @@ public class ClassFive {
 //			System.out.println(a[i]);
 //		}
 //		System.out.println("Average of the number entered" + (sum/5.0));
-		
+
 //		Scanner sc = new Scanner(System.in);
 //		int a[] = new int[5];
 //		int max = 0;
 //		for (int i = 0; i < 5; i++) { // 입력된 값 중 최댓값찾기
 //			a[i] = sc.nextInt();      // Find the maximum of 
-									  // the entered values
+		// the entered values
 //			if (max < a[i]) {
 //				max = a[i];
 //			}
 //		}
 //		System.out.println("The biggest number is " + max);
+
+//		int arr[]=new int[5];
+//		int size=arr.length; // 배열명.length
+//		System.out.println(size);
+
+//		Scanner sc = new Scanner(System.in);
+//		int arr[]=new int[5];
+//		double sum=0;
+//		for(int i=0;i<arr.length;i++) {
+//			arr[i]=sc.nextInt();
+//			sum+=arr[i];
+//		}
+//		System.out.println("The average is "+(sum/arr.length));
+
+//		Scanner sc = new Scanner(System.in);
+//		int arr[]=new int[7];
+//		int sum=0;
+//		for(int i=0;i<arr.length;i++) {
+//			arr[i]=sc.nextInt();
+//			sum+=arr[i];
+//		}
+//		System.out.println((double)sum/arr.length);
+
+//		int arr[]=new int[10]; //fibonacci
+//		arr[0]=1;
+//		arr[1]=2;
+//		for(int i=0;i<8;i++) {
+//			arr[i+2]=arr[i]+arr[i+1];
+//		}
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.println(arr[i]+" ");
+//		}
+		
+//		Scanner s = new Scanner(System.in);
+//		int sum = 0; // 총합변수
+//		int multi = 1; // 총곱변수
+//		int ary[] = new int[4];
+//		for(int i=0;i<ary.length;i++) {
+//			ary[i]=s.nextInt();
+//			sum+=ary[i];
+//			multi*=ary[i];
+//		}
+//		System.out.println("The Sum of the number entered is "+sum);
+//		System.out.println("Multiply all the entered numbers by "+multi);
+		
+//		int[] num = {1,2,3,4,5};
+//		for(int i=0;i<num.length;i++) {
+//			System.out.println(num[i]);
+//		}
+//		(향상된for문)for - each: 배열의 각요소를 순차적으로 접근한다
+//		for(int i:num) {
+//			System.out.println(i);
+//		}
+		
+//		String name[]= {"대저토마토","파인애플","바나나","사과","배",};
+//		for(String i:name) {
+//			System.out.println(i);
+//		}
+		
+//		int sum=0;
+//		int arr[]= {1,2,3,4,5};
+//		for(int i:arr) {
+//			sum+=i;
+//		}
+//		System.out.println(sum);
+		
+//		String str[]= {"computer","java","DB"};
+//		for(String s:str) {
+//			System.out.println(s);
+//		}
+		
+//		2차원 배열 행열
+//		int  arr[][];
+//		arr=new int[2][5];
+//		int arr[][] =new int[2][5];
+		
+//		char s[][] =new char[5][5];
+//		double d[][] =new double[5][2];
+//		
+//		int c[][] = {{1,2,3},{4,5,6}};
+//		for(int i=0;i<2;i++) {
+//			for(int j=0;j<3;j++) {
+//				System.out.println(c[i][j]);
+//			}
+//		}
+			
+//		Scanner sc = new Scanner(System.in);
+//		double arr[][]=new double[2][3];
+//		for(int i=0;i<2;i++) {
+//			for(int j=0;j<3;j++) {
+//				arr[i][j]=sc.nextDouble();
+//				System.out.println(arr[i][j]);
+//			}
+//		}
+		int a[]= new int[10];
+		Scanner sc =new Scanner(System.in);
+		for(int i=0;i<11;i++) {	
+			a[i]=sc.nextInt();
+			if(a[i]%5==0) {
+				System.out.println(a[i]);
+			}
+		}
+		
 		
 		
 

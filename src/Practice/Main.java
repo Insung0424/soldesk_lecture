b72a43242e4a03bb0d51458f90929a4f11c0b1e4
Insung0424 <zhrudrhks@gmail.com>
2022-05-13 18:10:30 +0900

    sixyh

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -1,27 +1,20 @@
 package Practice;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
-		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-        StringBuilder sb=new StringBuilder();
-        while(bf.readLine()==null) {
-            st  = new StringTokenizer(bf.readLine()," ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-		  	sb.append(a+b).append('\n');
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader bf =new BufferedReader(new InputStreamReader(System.in));
+		HashSet hs= new HashSet();
+		for(int i=0;i<10;i++) {
+			hs.add((Integer.parseInt(bf.readLine())%42));
 		}
-		System.out.println(sb);
+		System.out.println(hs.size());
+		
 	}
 
 }
